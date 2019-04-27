63ed8a6c08d0c7f7919dd4316572800742b8701e
ZachIsKing <zacharyhiscocks@juno.com>
2019-04-27 12:39:17 -0500

    Adds Coin script. Adds Coin 2DPhysics layer. Modifies the Physics collision matrix so coins don't collide with themselves.

---
+++ ldjam44/Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;
+        Debug.Log("collision");
+        Destroy(this.gameObject);
+    }
+}
