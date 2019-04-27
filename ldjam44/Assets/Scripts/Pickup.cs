a4b795129f0db57a924178636462c5e45c862a8c
ZachIsKing <zacharyhiscocks@juno.com>
2019-04-27 17:26:12 -0500

    Adds a basic powerup to test scene.

---
+++ ldjam44/Assets/Scripts/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;
+        Player playerScript = other.GetComponent<Player>();
+        if (playerScript)
+        {
+            ApplyPowerup(playerScript);
+        }
+        Destroy(this.gameObject);
+    }
+
+    public virtual void ApplyPowerup(Player player)
+    {
+    }
+}
---
+++ ldjam44/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : Pickup {
+
+    public PlayerStats powerup;
+
+    public override void ApplyPowerup(Player player)
+    {
+        PlayerStats playerStats = player.GetComponent<PlayerStats>();
+        playerStats.movementSpeed += powerup.movementSpeed;
+        playerStats.shotSpeed += powerup.shotSpeed;
+        playerStats.rateOfFire += powerup.rateOfFire;
+        playerStats.damage += powerup.damage;
+        playerStats.shotSize += powerup.shotSize;
+        playerStats.shotLifetime += powerup.shotLifetime;
+        playerStats.baseEffect.damage += powerup.damage;
+        if (powerup.baseEffect)
+        {
+            playerStats.baseEffect = powerup.baseEffect;
+        }
+
+        for (int i = 0; i < powerup.additionalEffects.Count; ++i)
+        {
+            playerStats.additionalEffects.Add(powerup.additionalEffects[i]);
+        }
+    }
+}
