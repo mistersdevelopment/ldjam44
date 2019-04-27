39687f0fffb3432f19e83d309d3bfe3f0a3a5902
msaladin <msaladin@bmt.world>
2019-04-27 10:46:27 -0500

    Added player gym.

---
+++ ldjam44/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float movementSpeed = 1.0f;
+
+	Rigidbody2D body;
+	private float horizontalMovement = 0;
+	private float verticalMovement = 0;
+
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		PerformMovement();
+	}
+
+	void PerformMovement()
+	{
+		horizontalMovement = Input.GetAxis("Horizontal");
+		verticalMovement = Input.GetAxis("Vertical");
+
+	}
+	void FixedUpdate()
+	{
+		body.velocity = new Vector2(horizontalMovement * movementSpeed, verticalMovement * movementSpeed);
+	}
+}
