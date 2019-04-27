094afb39fb87626c1f5a3650e79628cd5be6574c
msaladin <msaladin@bmt.world>
2019-04-27 12:20:31 -0500

    Very bad projectiles.

---
+++ ldjam44/Assets/Scripts/Direction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum CardinalDirection
+{
+	EAST,
+	NORTH,
+	WEST,
+	SOUTH
+}
+
+public static class DirectionUtils
+{
+	static Vector3[] directionVec = {
+			new Vector3( 1, 0 ),
+			new Vector3( 0, 1 ),
+			new Vector3( -1, 0 ),
+			new Vector3( 0, -1 )
+		};
+
+	public static Vector3 CardinalDirectionToVec(CardinalDirection dir)
+	{
+		return directionVec[(int)dir];
+	}
+
+	public static CardinalDirection CoordinatesToCardinalDirection(Vector2 dir)
+	{
+		return (CardinalDirection)CoordinatesToSector(dir, 4);
+	}
+
+	private static int CoordinatesToSector(Vector2 dir, int numSectors)
+	{
+		Vector2 norm = dir.normalized;
+		float angle = (float)Math.Atan2(norm.y, norm.x);
+		return (int)Math.Round(((numSectors * angle) / (2 * Math.PI)) + numSectors) % numSectors;
+	}
+}
--- ldjam44/Assets/Scripts/Player.cs
+++ ldjam44/Assets/Scripts/Player.cs
@@ -1,33 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
-	public float movementSpeed = 1.0f;
+	public Rigidbody2D coinPrefab;
+	
+	float movementSpeed = 2.0f;
+	float shotSpeed = 100f;
 
 	Rigidbody2D body;
 	private float horizontalMovement = 0;
 	private float verticalMovement = 0;
+	private CardinalDirection facing = CardinalDirection.SOUTH;
 
 	void Start()
 	{
 		body = GetComponent<Rigidbody2D>();
 	}
 
-	void Update()
+	void FixedUpdate()
 	{
-		PerformMovement();
+		body.velocity = new Vector2(horizontalMovement * movementSpeed, verticalMovement * movementSpeed);
 	}
 
-	void PerformMovement()
+	void Update()
+	{
+		Movement();
+		Attack();
+	}
+
+	void Movement()
 	{
 		horizontalMovement = Input.GetAxis("Horizontal");
 		verticalMovement = Input.GetAxis("Vertical");
 
+		Vector2 input = new Vector2(horizontalMovement, verticalMovement);
+		input.x = (float)(Math.Sign(input.x) * Math.Ceiling(Math.Abs(input.x)));
+		input.y = (float)(Math.Sign(input.y) * Math.Ceiling(Math.Abs(input.y)));
+		input = input.normalized;
+
+		if (input != Vector2.zero)
+		{
+			CardinalDirection dir = DirectionUtils.CoordinatesToCardinalDirection(input);
+
+			if (dir != facing)
+			{
+				facing = dir;
+			}
+		}
 	}
-	void FixedUpdate()
+
+	void Attack()
 	{
-		body.velocity = new Vector2(horizontalMovement * movementSpeed, verticalMovement * movementSpeed);
+		if (Input.GetButton("Fire1"))
+		{
+			Vector3 facingVec = DirectionUtils.CardinalDirectionToVec(facing);
+			Vector3 coinStart = transform.position + facingVec * 0.75f;
+			Rigidbody2D coin = Instantiate(coinPrefab, coinStart, Quaternion.identity) as Rigidbody2D;
+			coin.AddForce(facingVec * shotSpeed);
+		}
 	}
 }
