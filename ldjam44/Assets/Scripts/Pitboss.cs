fd0c11e88c65f57496c77c88e18ce82ddb8a300a
Joshua Brookover <joshua.al.brookover@gmail.com>
2019-04-28 15:46:25 -0500

    pitboss mechanics

--- ldjam44/Assets/Scripts/Pitboss.cs
+++ ldjam44/Assets/Scripts/Pitboss.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Pitboss : MonoBehaviour
 {
+    public float waitTime = 3;
+    public float chargeUpTime = 1;
+    public float chargeTime = 1;
+
     public Stats stats;
     Rigidbody2D body;
     Animator spritesAnimator;
@@ -11,8 +15,16 @@ public class Pitboss : MonoBehaviour
     bool moved = false;
     Vector2 movement = Vector2.zero;
 
+    float waitTimer;
+    float chargeUpTimer;
+    float chargeTimer;
+    Vector3 direction;
+
     void Start()
     {
+        waitTimer = waitTime;
+        chargeUpTimer = chargeUpTime;
+        chargeTimer = chargeTime;
         body = GetComponent<Rigidbody2D>();
         stats = GetComponent<Stats>();
         spritesAnimator = transform.Find("Sprites").GetComponent<Animator>();
@@ -20,37 +32,58 @@ public class Pitboss : MonoBehaviour
 
     void Update()
     {
-        // TODO Make granny randomly stop. Because tired.
         var player = GameObject.Find("Player");
+        movement = transform.position;
         if (player)
         {
-            var playerPos = player.transform.position;
-            movement = Vector2.MoveTowards(transform.position, playerPos, Time.deltaTime * stats.movementSpeed);
-            var grannyPos = transform.position;
-            grannyPos.z = 0;
-            var playerDir = Vector3.Normalize(playerPos - grannyPos);
-
-            var eastVal = Vector3.Dot(playerDir, DirectionUtils.CardinalDirectionToVec(CardinalDirection.EAST));
-            var westVal = Vector3.Dot(playerDir, DirectionUtils.CardinalDirectionToVec(CardinalDirection.WEST));
-            if (eastVal > westVal)
+            if (waitTimer > 0)
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                waitTimer -= Time.deltaTime;
+                spritesAnimator.SetBool("IsCharging", false);
+                spritesAnimator.SetBool("IsChargingUp", false);
+            }
+            else if (chargeUpTimer > 0)
+            {
+                chargeUpTimer -= Time.deltaTime;
+                if (chargeUpTimer <= 0)
+                {
+                    direction = Vector3.Normalize(player.transform.position - transform.position);
+                    chargeTimer = chargeTime;
+                }
+                spritesAnimator.SetBool("IsChargingUp", true);
             }
             else
             {
-                transform.localScale = new Vector3(-1, 1, 1);
-            }
+                spritesAnimator.SetBool("IsCharging", true);
+                chargeTimer -= Time.deltaTime;
+                if (chargeTimer < 0)
+                {
+                    waitTimer = waitTime;
+                    chargeUpTimer = chargeUpTime;
+                }
 
-            moved = true;
-            spritesAnimator.SetBool("IsWalking", moved);
+                movement = Vector2.MoveTowards(transform.position, transform.position + direction, Time.deltaTime * stats.movementSpeed);
+
+                if (movement.x > 0)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+
+                //spritesAnimator.SetBool("IsWalking", moved);
+            }
         }
+        moved = true;
     }
 
     void FixedUpdate()
     {
         if (moved)
         {
-            //body.MovePosition(movement);
+            body.MovePosition(movement);
         }
     }
 }
