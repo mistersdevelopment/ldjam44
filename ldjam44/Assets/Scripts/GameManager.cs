bc72df9871a467c84fb0e66729b8319a383277a2
Tim Ullrich <timothy.ullrich@gmail.com>
2019-04-28 10:58:03 -0500

    Add top level Game scene (which will load in rooms) and GameManager class

---
+++ ldjam44/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void BeginGameplay()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
