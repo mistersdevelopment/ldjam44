e2dbecfa8cc8b48e6b2c7bdf97d66744ef5242b3
Tim Ullrich <timothy.ullrich@gmail.com>
2019-04-27 22:11:03 -0500

    Fix compile errors

--- ldjam44/Assets/Scripts/BurningShotEffect.cs
+++ ldjam44/Assets/Scripts/BurningShotEffect.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BurningShotEffect : AdditionalCoinEffect
+public class BurningShotEffect : Effect
 {
 
-    public virtual void ApplyEffect(Enemy enemy)
+    public virtual void ApplyEffect(Health enemy)
     {
 
     }
