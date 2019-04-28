e07c6af5aacdb5817da49946e994e6f22b15a569
ZachIsKing <zacharyhiscocks@juno.com>
2019-04-27 19:01:27 -0500

    Adds the main menu, and help scenes.

---
+++ ldjam44/Assets/Scripts/UINavigator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UINavigator : MonoBehaviour
+{
+    public void GoToMenuScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void GoToGameScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+    
+    public void ContinuePlaying()
+    {
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(3);
+    }
+
+    public void OpenHelp()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+
+    public void CloseHelp()
+    {
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
