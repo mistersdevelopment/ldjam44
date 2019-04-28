1674b4383f41bc4f3aebfa5ac4246822a15566d9
Joshua Brookover <joshua.al.brookover@gmail.com>
2019-04-28 15:23:44 -0500

    Add scripts for each enemy

---
+++ ldjam44/Assets/Scripts/Pitboss.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pitboss : MonoBehaviour
+{
+    public Stats stats;
+    Rigidbody2D body;
+    Animator spritesAnimator;
+
+    bool moved = false;
+    Vector2 movement = Vector2.zero;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        stats = GetComponent<Stats>();
+        spritesAnimator = transform.Find("Sprites").GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // TODO Make granny randomly stop. Because tired.
+        var player = GameObject.Find("Player");
+        if (player)
+        {
+            var playerPos = player.transform.position;
+            movement = Vector2.MoveTowards(transform.position, playerPos, Time.deltaTime * stats.movementSpeed);
+            var grannyPos = transform.position;
+            grannyPos.z = 0;
+            var playerDir = Vector3.Normalize(playerPos - grannyPos);
+
+            var eastVal = Vector3.Dot(playerDir, DirectionUtils.CardinalDirectionToVec(CardinalDirection.EAST));
+            var westVal = Vector3.Dot(playerDir, DirectionUtils.CardinalDirectionToVec(CardinalDirection.WEST));
+            if (eastVal > westVal)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            moved = true;
+            spritesAnimator.SetBool("IsWalking", moved);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (moved)
+        {
+            //body.MovePosition(movement);
+        }
+    }
+}
